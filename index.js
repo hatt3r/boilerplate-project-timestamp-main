439719a4adc10547c9630d7d03ac9eeaab907476
Serene Mathew <46328829+hatt3r@users.noreply.github.com>
2024-03-24 19:39:05 +0530

    comments

--- index.js
+++ index.js
@@ -21,25 +21,41 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/:date?", (req, res) => {
+
+  //dateParam variable will contain request paramters date that is provided in the path
   const dateParam = req.params.date; 
+  //we initialize the dateObject 
   let dateObject;
   
+  //if dateParam variable is null or is does not contain anything 
+  // then we provide the dateObject with new Date()
   if(!dateParam)
   {
     dateObject = new Date()
   }
+  //else if contains a date it must be in unix time format(5 or more digits)
+  //hence we check for only the digits
+  //we parse those digits as new date and convert to dateObject
   else if(/\d{5,}/.test(dateParam))
   {
     dateObject = new Date(parseInt(dateParam))
   }
+  //if dateParams contain normal date integers then just use new Date() function
+  // to give dateObject the date
   else 
   {
     dateObject = new Date(dateParam)
   }
 
+
+  //if the time in the dateObject is not a number or integer
+  //then response json will have error: "Invalid Date"
   if (isNaN(dateObject.getTime())) {
     return res.json({ error : "Invalid Date" }); 
   }
+  //else the response json will have
+  //unix as key and value as dateObject.getTime() time in unix format
+  //utc as key and value as dateObject.toUTCSting() convert it to UTC
   else {
     res.json({unix:dateObject.getTime(),utc:dateObject.toUTCString()})
   }
